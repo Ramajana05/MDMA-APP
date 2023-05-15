9d11543b12bafda0100eb8c560a7da13266b0f44
Mittmat AB <mittmat.se@gmail.com>
2023-05-15 13:09:05 +0200

    statistics implemented as line charts  with checkboxes

--- lib/main.dart
+++ lib/main.dart
@@ -4,21 +4,26 @@ import 'package:forestapp/screen/damagesDashboardScreen.dart';
 import 'package:forestapp/screen/dashboardScreen.dart';
 import 'package:forestapp/screen/loginScreen.dart';
 import 'package:forestapp/screen/damageReport.dart';
+import 'package:forestapp/screen/statisticsScreenBalkenCHarts.dart';
 import 'package:forestapp/widget/bottomNavBar.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:wakelock/wakelock.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  Wakelock.enable();
+
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-        statusBarColor: Color.fromARGB(0, 231, 229, 229),
-        systemNavigationBarColor: Color.fromARGB(0, 255, 255, 255)));
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+        statusBarColor: Colors.white, systemNavigationBarColor: Colors.white));
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: LoginPage(),
