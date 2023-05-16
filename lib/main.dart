4dbd95411ef8090f02acd30b5c964473e4d04f29
Ramajana Skopljak <skopljak@stud.hs-heilbronn.de>
2023-05-16 14:06:41 +0200

    updated main for db

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,64 @@ import 'package:sqflite/sqflite.dart';
 
 import 'package:forestapp/service/loginService.dart';
 
-void main() {
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+import 'dart:io';
+import 'dart:convert';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // Initialize the database
+  await initDatabase();
+
   runApp(MyApp());
+}
+
+Future<void> initDatabase() async {
+  try {
+    final documentsDirectory = await getApplicationDocumentsDirectory();
+    final path = join(documentsDirectory.path, 'MDMADatabase.db');
+
+    final databaseExists = await File(path).exists();
+
+    if (!databaseExists) {
+      final dbAssetPath = await getDatabaseAssetPath();
+      await copyDatabase(dbAssetPath, path);
+    }
+  } catch (e) {
+    // Handle any errors that occur during database initialization
+    print('Database initialization failed: $e');
+    rethrow;
+  }
+}
+
+Future<String> getDatabaseAssetPath() async {
+  try {
+    final manifestContent = await rootBundle.loadString('AssetManifest.json');
+    final manifest = json.decode(manifestContent) as Map<String, dynamic>;
+    final dbPath = manifest.keys
+        .firstWhere((key) => key.contains('assets/database/MDMADatabase.db'));
+    return dbPath;
+  } catch (e) {
+    // Handle any errors related to loading the asset manifest
+    print('Error loading database asset path: $e');
+    rethrow;
+  }
+}
+
+Future<void> copyDatabase(String dbAssetPath, String dbPath) async {
+  try {
+    final byteData = await rootBundle.load(dbAssetPath);
+    final bytes = byteData.buffer.asUint8List();
+
+    await File(dbPath).writeAsBytes(bytes);
+  } catch (e) {
+    // Handle any errors related to copying the database
+    print('Error copying database: $e');
+    rethrow;
+  }
 }
 
 class MyApp extends StatelessWidget {
