4d23e07b8d4756f81f718f62c0a84c9b6be339b9
Ramajana Skopljak <skopljak@stud.hs-heilbronn.de>
2023-06-13 23:09:51 +0200

    fixed bug

--- lib/dialog/logoutDialog.dart
+++ lib/dialog/logoutDialog.dart
@@ -7,56 +7,70 @@ class LogoutDialog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AlertDialog(
-      title: Text(
-        'Ausloggen',
-        style: LogOutDecoration.getTitleTextStyle(),
-      ),
-      content: Row(
-        children: [
-          //Icon(Icons.warning, color: Colors.orange),
-          SizedBox(width: 8.0),
-          Text('Wirklich ausloggen?'),
+    final screenWidth = MediaQuery.of(context).size.width;
+    final dialogWidth = screenWidth * 0.9;
+
+    return Container(
+      width: dialogWidth,
+      child: AlertDialog(
+        title: Text(
+          'Ausloggen',
+          style: LogOutDecoration.getTitleTextStyle().copyWith(fontSize: 24.0),
+        ),
+        content: Padding(
+          padding: EdgeInsets.symmetric(vertical: 16.0),
+          child: Row(
+            children: [
+              //Icon(Icons.warning, color: Colors.orange),
+              SizedBox(width: 8.0),
+              Text(
+                'Wirklich ausloggen?',
+                style: TextStyle(
+                  fontSize: 20.0,
+                ),
+              ),
+            ],
+          ),
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).popUntil((route) => route.isFirst);
+            },
+            child: Text(
+              'Abbrechen',
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 20.0,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => LoginPage()),
+              );
+            },
+            child: Text('Ausloggen', style: TextStyle(fontSize: 20.0)),
+            style: ElevatedButton.styleFrom(
+              primary: Color.fromARGB(255, 40, 233, 127),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12.0),
+              ),
+            ),
+          ),
         ],
-      ),
-      actions: [
-        TextButton(
-          onPressed: () {
-            Navigator.of(context).popUntil((route) => route.isFirst);
-          },
-          child: Text(
-            'Abbrechen',
-            style: TextStyle(
-              color: Colors.grey, // Set the text color to green
-              fontSize: 14.0,
-              fontWeight: FontWeight.w500,
-            ),
-          ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(9.0),
         ),
-        ElevatedButton(
-          onPressed: () {
-            Navigator.pushReplacement(
-              context,
-              MaterialPageRoute(builder: (context) => LoginPage()),
-            ); // Close the dialog
-          },
-          child: Text('Ausloggen'),
-          style: ElevatedButton.styleFrom(
-            primary: Color.fromARGB(255, 40, 233, 127),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(12.0),
-            ),
-          ),
+        backgroundColor: Colors.white,
+        elevation: 8.0,
+        contentPadding: EdgeInsets.symmetric(
+          horizontal: 16.0,
+          vertical: 8.0,
         ),
-      ],
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(9.0),
-      ),
-      backgroundColor: Colors.white,
-      elevation: 8.0,
-      contentPadding: EdgeInsets.symmetric(
-        horizontal: 16.0,
-        vertical: 8.0,
       ),
     );
   }
