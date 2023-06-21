b608da7a02113a23d773c9d65be5613cc5292ac9
Ayashiina <anima5578@gmail.com>
2023-06-21 22:06:53 +0200

    delete javadoc useless one

--- lib/colors/appColors.dart
+++ lib/colors/appColors.dart
@@ -30,3 +30,98 @@ const primaryHumidityShadowColor = Color.fromARGB(255, 196, 236, 255);
 
 ///Battery green color
 const primaryGreen = Color.fromARGB(255, 46, 202, 51);
+const orangeAccent = Colors.orangeAccent;
+const orange = Colors.orange;
+
+/// Bottom Nav Bar
+const bottomGreen = Color.fromARGB(255, 41, 235, 15);
+const bottomGradient = [
+  Color(0xFF2DFFD9),
+  Color(0xFF00FF57),
+];
+const darkGreen = Color.fromARGB(204, 0, 165, 22);
+final List<Color> tabColors = [
+  darkGreen, // Dashboard
+  blue, // Statistics
+  red, // Map
+  blue, // QR Code
+  darkGreen, // Sensors
+];
+Color bottomNavSelectColor = getBottomNavColor();
+
+/// Logout Dialog
+const logoutBlack = Colors.black26;
+
+///Profile
+Color iconLightRed = passwordIconColor();
+Color textLightRed = passwordChangeColor();
+const lightRed = Color.fromARGB(255, 255, 161, 152);
+
+///TopNavBar
+const topGreen = Color.fromARGB(255, 86, 252, 108);
+const topBorderGreen = Color.fromARGB(6, 95, 247, 115);
+Color invertedColor = invertedColour();
+Color textInverted = getInvertedTextColor();
+Color buttonTextInversedColor = getButtonTexInversedColor();
+Color buttonTextColor = getButtonTextColor();
+
+Color getTextColor() {
+  return isNightMode ? black : white;
+}
+
+Color getInvertedTextColor() {
+  return isNightMode ? white : black;
+}
+
+Color getButtonTextColor() {
+  return isNightMode ? grey : white;
+}
+
+Color getBottomNavColor() {
+  return isNightMode ? black : grey.shade100;
+}
+
+Color getButtonTexInversedColor() {
+  return isNightMode ? white : grey;
+}
+
+Color iconColor() {
+  return isNightMode ? black : yellow;
+}
+
+Color passwordChangeColor() {
+  return isNightMode ? red : lightRed;
+}
+
+Color passwordIconColor() {
+  return isNightMode ? red : lightRed;
+}
+
+Color changeBackground() {
+  return isNightMode ? white : darkBackground;
+}
+
+Color changeBackgroundLighter() {
+  return isNightMode ? white : grey.shade600;
+}
+
+Color invertedColour() {
+  return isNightMode ? black.withOpacity(0.5) : black;
+}
+
+bool isNightMode = true;
+
+void toggleNightMode() {
+  isNightMode = !isNightMode; // Toggle the value of isNightMode
+  textColor = getTextColor(); // Update text color based on isNightMode
+  invertedColor = invertedColour();
+  background =
+      changeBackground(); // Update background color based on isNightMode
+  lighterBackground = changeBackgroundLighter();
+  textInverted = getInvertedTextColor();
+  iconLightRed = passwordIconColor();
+  textLightRed = passwordChangeColor();
+  buttonTextInversedColor = getButtonTexInversedColor();
+  buttonTextColor = getButtonTextColor();
+  bottomNavSelectColor = getBottomNavColor();
+}
