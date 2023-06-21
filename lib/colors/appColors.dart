5eb571a2c3df19c5defded9e0bfd9975fb6ee958
Ayashiina <anima5578@gmail.com>
2023-06-21 14:27:00 +0200

    Update appColors.dart

--- lib/colors/appColors.dart
+++ lib/colors/appColors.dart
@@ -31,6 +31,54 @@ const primarygrey = Color.fromARGB(255, 134, 134, 134);
 const primaryWarningOrange = Color.fromARGB(255, 255, 106, 37);
 const primaryNewsBlue = Color.fromARGB(255, 37, 70, 255);
 const primarySignalStrength = Colors.black;
+const onlineColor = Color.fromARGB(255, 64, 236, 73);
+
+///Box Shadow Color
+var boxshadowColor = Color.fromARGB(255, 158, 158, 158).withOpacity(0.5);
+
+///Marker Color
+const markerColor = Color.fromARGB(255, 56, 162, 197);
+
+/// Set the color of the icon
+const iconColor = Color.fromARGB(255, 154, 155, 154);
+const dialogIconColor = Color.fromARGB(255, 173, 173, 173);
+
+/// SidePanel Gradient
+const gradient = [
+  Color.fromARGB(255, 86, 252, 108),
+  Color.fromARGB(255, 40, 233, 127)
+];
+const blue = Color.fromARGB(255, 7, 19, 29);
+
+///Dashboard Color
+const lightGreen = Color.fromARGB(255, 194, 255, 241);
+const lightRed = Color.fromARGB(255, 255, 199, 199);
+const lightBlue = Color.fromARGB(255, 196, 236, 255);
+const darkBlue = Color.fromARGB(255, 37, 70, 255);
+const darkRed = Color.fromARGB(255, 255, 106, 37);
+
+/// Help
+const grey = Color.fromARGB(255, 127, 127, 128);
+const red = Color.fromARGB(255, 255, 2, 2);
+const green = Color.fromARGB(255, 58, 243, 33);
+
+/// Map
+const mapGreen = Color.fromARGB(255, 58, 216, 10);
+const mapBlue = Color.fromARGB(255, 0, 112, 204);
+
+/// Profile
+const profileBlack = Color.fromARGB(255, 24, 23, 23);
+
+///Statistic
+const statColor = Color.fromRGBO(38, 158, 38, 0.2);
+
+/// Bottom Nav
+const darkGreen = Color.fromARGB(204, 0, 165, 22);
+const sensorGreen = Color.fromARGB(204, 0, 165, 22);
+
+///Warning Widet
+const backgroundColor = Color.fromARGB(255, 248, 250, 253);
+var opacityGray = const Color.fromARGB(255, 170, 170, 170).withOpacity(0.5);
 // Light-Dark Mode Colors------------------------------
 // Shared Colors
 var primarybackgroundColor = Colors.white;
